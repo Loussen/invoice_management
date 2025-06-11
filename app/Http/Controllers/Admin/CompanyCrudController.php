81b7138ba9c95f35751ef50eb90f7e35c99c8023
Fuad Hasanli <fhesenli92@gmail.com>
2025-06-11 14:56:21 +0400

    Update CompanyCrudController.php

--- app/Http/Controllers/Admin/CompanyCrudController.php
+++ app/Http/Controllers/Admin/CompanyCrudController.php
@@ -64,6 +64,8 @@ class CompanyCrudController extends CrudController
      */
     protected function setupListOperation()
     {
+        CRUD::disableResponsiveTable();
+        CRUD::setPageLengthMenu([[25, 50, 100, 300, 500, 3000, 5000 -1], [25, 50, 100, 300, 500, 3000, 5000, "backpack::crud.all"]]);
         CRUD::column('name')->label('Company name');
         CRUD::column('iban')->label('IBAN');
         CRUD::addColumn([
